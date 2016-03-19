22e9eacdf9a1a162608bbbf8d476636b85f4deeb
Tim Officer <timothyofficer@gmail.com>
2016-03-18 23:02:01 -0400

    features

--- src/js/components/home/homeCtrl.js
+++ src/js/components/home/homeCtrl.js
@@ -3,7 +3,11 @@ angular.module('mongotronApp').controller('homeCtrl', [
   function($scope) {
     $scope.features = [{
       name: 'Replica Sets',
-      description: 'Mongotron supports connecting to replica sets. When adding a new connection simply enable the replica sets options and all the server that belong to the set.',
+      description: 'Mongotron supports connecting to replica sets. When adding a new connection simply enable the replica sets option and add all the servers that belong to the set.',
+      imageUrl: './src/images/features/replsets.png'
+    }, {
+      name: 'Replica Sets',
+      description: 'Mongotron supports connecting to replica sets. When adding a new connection simply enable the replica sets option and add all the servers that belong to the set.',
       imageUrl: './src/images/features/replsets.png'
     }];
   }
